6fb0a70f66744a399aac8a93bd60f41aec4c1421
Gabriel <gabrielsrmj@alu.ufc.br>
2024-07-24 22:45:24 -0300

    Entidades feitas e relações já feitas

---
+++ src/main/java/com/forum/entitys/Topico.java
@@ -0,0 +1,40 @@
+package com.forum.entitys;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity(name = "topico")
+@Table(name = "topico")
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+public class Topico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private LocalDateTime dataPostagem;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+    @OneToMany(mappedBy = "topico")
+    private Set<CurtidasTopicos> curtidasTopicos;
+}
---
+++ src/main/java/com/forum/entitys/Usuario.java
@@ -0,0 +1,45 @@
+package com.forum.entitys;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity(name = "usuario")
+@Table(name = "usuario")
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @Column(unique = true)
+    private String login;
+    private String senha;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Resposta respostas;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Topico topicos;
+
+    @OneToMany(mappedBy = "usuario")
+    private Set<CurtidasTopicos> curtidasTopicos;
+
+    @OneToMany(mappedBy = "usuario")
+    private Set<CurtidasRespostas> curtidasRespostas;
+}
