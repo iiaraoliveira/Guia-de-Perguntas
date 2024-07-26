da1095344da447f07571c7ceaee6387dd9a162bc
Gabriel <gabrielsrmj@alu.ufc.br>
2024-07-26 18:45:52 -0300

    Consertando um erro no repository do Tópico

--- src/main/java/com/forum/repositorys/TopicoRepository.java
+++ src/main/java/com/forum/repositorys/TopicoRepository.java
@@ -1,8 +1,9 @@
 package com.forum.repositorys;
 
+import com.forum.entitys.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TopicoRepository extends JpaRepository<Repository, String>{
+public interface TopicoRepository extends JpaRepository<Topico, String>{
 }
