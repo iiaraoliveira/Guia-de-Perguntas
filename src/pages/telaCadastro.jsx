5ff1303814f6f6e7b0b39cb7a4bab051f64ca85e
raquel.duarte <raquelduarteluis@gmail.com>
2024-08-01 06:14:24 -0300

    troquei a classe textfield por textfieldCadastro, adicionei usuário, troquei as cores

--- src/pages/telaCadastro.jsx
+++ src/pages/telaCadastro.jsx
@@ -7,16 +7,19 @@ const TelaCadastro = () => {
         <div className="main-cadastro">
             <div className="left-cadastro">
                 <h1>Faça um cadastro<br/>E entre para nosso time</h1>
-                <img src="hand-coding-animate.svg" className="left-cadastro-image" alt="Codando"/>
             </div>
             <div className="right-cadastro">
                 <div className="card-cadastro">
-                    <h1>cadastro</h1>
-                    <div className="textfield">
+                    <h1>SING UP</h1>
+                    <div className="textfieldCadastro">
                         <label for="usuario">Usuário</label>
                         <input type="text" name="usuario" placeholder="Usuário"/>
                     </div>
-                    <div className="textfield">
+                    <div className="textfieldCadastro">
+                        <label for="email">Email</label>
+                        <input type="text" name="email" placeholder="Email"/>
+                    </div>
+                    <div className="textfieldCadastro">
                         <label for="password">Senha</label>
                         <input type="password" name="senha" placeholder="Senha"/>   
                     </div>
