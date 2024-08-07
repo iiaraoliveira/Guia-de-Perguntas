f62c87cf230379df9f53d2a14855a04add416c97
VITORIA NOBRE LIMA <1-05985@INTA.LOCAL>
2024-08-07 11:12:09 -0300

    correção de bugs antes da integração

--- backnode/database/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const connection = new Sequelize('guia-de-perguntas', 'root', '43215', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-module.exports = connection;
